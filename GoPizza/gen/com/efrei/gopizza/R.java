f1eaa5ed6e5eb6110aa534e27625c2ec88d4b9dc
Romain <romaincourtieux@hotmail.com>
2015-04-21 01:11:43 +0200

    Création des fichiers xml de layout

--- GoPizza/gen/com/efrei/gopizza/R.java
+++ GoPizza/gen/com/efrei/gopizza/R.java
@@ -31,6 +31,12 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     }
     public static final class layout {
         public static final int activity_fullscreen=0x7f030000;
+        public static final int gopizzafast_fullscreen=0x7f030001;
+        public static final int gopizzaregular_fullscreen=0x7f030002;
+        public static final int loading_fullscreen=0x7f030003;
+        public static final int news_fullscreen=0x7f030004;
+        public static final int reception_fullscreen=0x7f030005;
+        public static final int settings_fullscreen=0x7f030006;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
